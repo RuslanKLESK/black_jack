f586bd24f012d6c993b1708189b525a8261fc8da
RuslanKLESK <83598714+RuslanKLESK@users.noreply.github.com>
2021-08-09 19:40:22 +0300

    Add files via upload

---
+++ user.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# User Class
+
+class User < Players
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    super()
+  end
+
+  def deal_cards
+    super()
+    user_card_info
+    puts "Ставки сделаны. Ваш баланс: #{amount}"
+  end
+
+  def user_card_info
+    @points = 0
+    show_cards(:user)
+    puts "Колличество очков: #{points}"
+  end
+  
+  def add_card
+    super()
+    user_card_info
+  end
+
+end
